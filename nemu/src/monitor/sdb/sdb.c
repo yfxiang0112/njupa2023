f7c84b1855ba942efe6cc7856d6ade480fde3eff
tracer-ics2023 <tracer@njuics.org>
2023-09-14 10:06:29 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:06:29 up  1:19,  4 users,  load average: 0.15, 0.16, 0.21

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -55,7 +55,8 @@ static int cmd_q(char *args) {
 
 static int cmd_si(char *args){
 	//TODO sdb_command: si[nu]
-	char step = args[0];
+	int step = args[0]-'0';
+	//TODO handle undesired arg input
 	for (int i=0; i<step; i++){
 		cpu_exec(1);
 	}
