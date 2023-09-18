b28ad6ae1beca8dc9c5f61e68638f00558f2fb98
tracer-ics2023 <tracer@njuics.org>
2023-09-18 20:15:26 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:15:26 up 3 days,  6:57,  6 users,  load average: 0.26, 0.17, 0.20

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -120,12 +120,12 @@ void display_wp() {
 	WP *curr = head;
 	if (head != NULL) {
 		while (curr!=NULL) {
-			printf("-------\nwatchpoint No.%d \n", curr->NO);
+			printf("watchpoint No.%d \n", curr->NO);
 			curr = curr->next;
 		}
 	}
 
-	printf("TEMP test for free wp pool:\n");
+	printf("----------\nTEMP test for free wp pool:\n");
 	curr = free_;
 	if (free_ != NULL) {
 		while (curr!=NULL) {
