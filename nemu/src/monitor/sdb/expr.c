e0a11dfa848e8cf83820b7836dce4c66b9c733f3
tracer-ics2023 <tracer@njuics.org>
2023-09-18 08:54:06 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:54:06 up 2 days, 19:35,  5 users,  load average: 0.48, 0.25, 0.16

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -217,10 +217,13 @@ static word_t eval_expr(int p, int q) {
 	/* other cases, invalid expr. */
 	else {
 		//printf("Invalid expression. Please input a valid expression.\n");
+
+		printf("p:%d, q:%d\n", p, q);
 		for (int i=p; i<=q; i++) {
 			printf("%s", tokens[i].str);
 		}
 		printf("\n");
+
 		assert(0);
 		printf("0");
 		return 0;
