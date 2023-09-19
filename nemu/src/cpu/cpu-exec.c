8f5773a55798bd5d5574953842a96f86235ec955
tracer-ics2023 <tracer@njuics.org>
2023-09-19 10:50:34 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:50:34 up 3 days, 21:32,  6 users,  load average: 0.17, 0.22, 0.19

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -42,7 +42,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
 #ifdef CONNFIG_WATCHPOINT 
-	if (WATCHPOINT) {
+	if (WATCHPOINT == y) {
 		if (scan_wp()) {
 			nemu_state.state = NEMU_STOP;
 		}
