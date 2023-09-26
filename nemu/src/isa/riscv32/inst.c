3b65e89482aa7fd2fb116f4b91c5a622958338fc
tracer-ics2023 <tracer@njuics.org>
2023-09-26 20:56:35 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:56:35 up 12:23, 10 users,  load average: 0.39, 0.13, 0.09

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -23,7 +23,7 @@
 #define Mw vaddr_write
 
 enum {
-  TYPE_I, TYPE_S, TYPE_U, TYPE_J,
+  TYPE_I, TYPE_S, TYPE_B, TYPE_U, TYPE_J,
   TYPE_N, // none
 };
 
@@ -31,6 +31,7 @@ enum {
 #define src2R() do { *src2 = R(rs2); } while (0)
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
+#define immB() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 12) | (BITS(i, 7, 7) << 11) | (BITS(i, 30, 25) << 5) | (BITS(i, 11, 8) << 1); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | (BITS(i, 19, 12) << 12) | (BITS(i, 20, 20) << 11) | (BITS(i, 30, 25) << 5) | (BITS(i, 24, 21) << 1); } while(0)
 
@@ -42,6 +43,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
   switch (type) {
     case TYPE_I: src1R();          immI(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
+    case TYPE_B: src1R(); src2R(); immB(); break;
     case TYPE_U:                   immU(); break;
 		case TYPE_J: 									 immJ(); break;
   }
@@ -65,6 +67,7 @@ static int decode_exec(Decode *s) {
 	/* TODO: the function of this line is UNTESTED !!! might be wrong. */
   INSTPAT("??????? ????? ????? ??? ????? 11001 11", jalr   , I, s->dnpc = (src1 + imm) >>1 <<1; R(rd) = s->snpc);
 
+  INSTPAT("??????? ????? ????? 100 ????? 11000 11", blt    , B, if((int)src1<(int)src2) { s->dnpc = s->pc + imm; });
   INSTPAT("??????? ????? ????? 010 ????? 00000 11", lw     , I, R(rd) = Mr(src1 + imm, 4));
   INSTPAT("??????? ????? ????? 100 ????? 00000 11", lbu    , I, R(rd) = Mr(src1 + imm, 1));
   INSTPAT("??????? ????? ????? 000 ????? 01000 11", sb     , S, Mw(src1 + imm, 1, src2));
