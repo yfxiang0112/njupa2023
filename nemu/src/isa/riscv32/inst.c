d608df9a3e39b3121d282f59bfd2df3a3443f46a
tracer-ics2023 <tracer@njuics.org>
2023-09-26 21:08:42 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:08:42 up 12:35, 10 users,  load average: 0.10, 0.15, 0.10

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -23,7 +23,7 @@
 #define Mw vaddr_write
 
 enum {
-  TYPE_I, TYPE_S, TYPE_B, TYPE_U, TYPE_J,
+  TYPE_R, TYPE_I, TYPE_S, TYPE_B, TYPE_U, TYPE_J,
   TYPE_N, // none
 };
 
@@ -41,6 +41,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
   int rs2 = BITS(i, 24, 20);
   *rd     = BITS(i, 11, 7);
   switch (type) {
+		case TYPE_R: src1R(); src2R();         break;
     case TYPE_I: src1R();          immI(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
     case TYPE_B: src1R(); src2R(); immB(); break;
@@ -73,6 +74,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? 000 ????? 01000 11", sb     , S, Mw(src1 + imm, 1, src2));
   INSTPAT("??????? ????? ????? 010 ????? 01000 11", sw     , S, Mw(src1 + imm, 4, src2));
   INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi   , I, R(rd) = src1 + imm);
+  INSTPAT("0000000 ????? ????? 010 ????? 01100 11", slt    , R, if((int)src1<(int)src2) { R(rd) = 0x1; } else { R(rd) = 0x0; });
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
