fa8e8abdcf7b737337505436550dae71f82154de
tracer-ics2023 <tracer@njuics.org>
2023-09-27 16:59:36 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:59:36 up  3:13,  4 users,  load average: 0.54, 0.56, 0.55

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -32,7 +32,7 @@ void isa_reg_display() {
 
 word_t isa_reg_str2val(const char *s, bool *success) {
 	for (int i=0; i<32; i++){
-		if (strcmp(s, regs[i])==0) { return cpu.gpr[i]; }
+		if (strcmp(s, reg_name(i))==0) { return cpu.gpr[i]; }
 	}
 	if (strcmp(s, "pc")==0) { return cpu.pc; }
 	*success = false;
