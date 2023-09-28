2e7f53fa97d21b45c7823d40e41f844b9129b14f
tracer-ics2023 <tracer@njuics.org>
2023-09-28 19:28:16 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:28:16 up 10:52,  6 users,  load average: 0.31, 0.35, 0.23

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -100,7 +100,7 @@ static int cmd_x(char *args){
 	char *addr_str = strtok(NULL, " ");
 
 	/* turn into int. */
-	printf("%s\n", len_str);
+	printf("%ld\n", strtol(len_str, &endptr, 16));
 	printf("-len2 %s", addr_str);
 	if (strlen(addr_str)==0) {
 		addr = strtol(len_str, &endptr, 16);
