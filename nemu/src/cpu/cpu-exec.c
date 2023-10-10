55c866c179b6e51c1178d52b29fd17f87bc413a3
tracer-ics2023 <tracer@njuics.org>
2023-10-10 20:24:47 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:24:47 up  4:21,  6 users,  load average: 0.10, 0.13, 0.10

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -67,14 +67,11 @@ void quit_trace() {
 #ifdef CONFIG_ITRACE_COND
 			if (ITRACE_COND) {
 				if (nemu_state.halt_ret != 0) {
-					log_write("IRINGBUF:\n");
-					printf("IRINGBUF:\n");
+					Log("IRINGBUF:\n");
 					for (int i=0; i<I_TRACE_BUF_LEN; i++) {
-						log_write("%s\n",iringbuf[i]);
-						printf("%2d: %s\n", i+1, iringbuf[i]);
+						Log("%s\n",iringbuf[i]);
 					}
-					log_write("curr ring:%d\n", ringidx);
-					printf("curr ring:%d\n", ringidx);
+					Log("curr ring:%d\n", ringidx);
 				}
 			}
 #endif
