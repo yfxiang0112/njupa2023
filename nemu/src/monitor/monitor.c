73a54773bf83962740f43a352ce3346ef10a26a4
tracer-ics2023 <tracer@njuics.org>
2023-10-10 23:13:27 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:13:27 up  7:10, 12 users,  load average: 0.17, 0.30, 0.38

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -34,7 +34,7 @@ static void welcome() {
   Log("Build time: %s, %s", __TIME__, __DATE__);
   printf("Welcome to %s-NEMU!\n", ANSI_FMT(str(__GUEST_ISA__), ANSI_FG_YELLOW ANSI_BG_RED));
   printf("For help, type \"help\"\n");
-	printf(ANSI_FMT("al",ANSI_BG_BLACK)ANSI_FMT("ways coding with PRIDE!\n", ANSI_FG_RED));
+	printf(ANSI_FMT("alw",ANSI_FG_WHITE)ANSI_FMT("ays",ANSI_FG_BLACK)ANSI_FMT(" coding with PRIDE!\n", ANSI_FG_RED));
 }
 
 #ifndef CONFIG_TARGET_AM
