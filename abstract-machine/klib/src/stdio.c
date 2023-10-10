bbe707a685794db0b31f9976d835f20a10895491
tracer-ics2023 <tracer@njuics.org>
2023-10-10 10:11:49 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:11:49 up  1:49,  4 users,  load average: 0.39, 0.29, 0.17

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,6 +5,29 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+char* itoa(int num, char* buf, size_t base) {
+	if (num==0) {
+		*buf = 0;
+		*(buf+1) = '\0';
+		return buf;
+	}
+
+	if (num<0) {
+		*buf = '-';
+		buf ++;
+		num = -num;
+	}
+
+	while (num >0) {
+		*buf = num%base + '0';
+		buf ++;
+		num /= base;
+	}
+	*buf = '\0';
+
+	return buf;
+}
+
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
@@ -14,7 +37,55 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+	va_list ap;
+	int d;
+	char *s;
+
+	char buf[65];
+	int cnt = 0;
+
+	va_start(ap, fmt);
+	while (*fmt != '\0') {
+
+		if (*fmt == '%') {
+			fmt++;
+
+			switch (*fmt) {
+				case 'd':
+					d = va_arg(ap, int);
+
+					itoa(d, buf, 10);
+					memcpy(out, buf, strlen(buf));
+					out += strlen(buf);
+					cnt += strlen(buf);
+					break;
+
+				case 's':
+					s = va_arg(ap, char*);
+					memcpy(out, s, strlen(s));
+					out += strlen(s);
+					cnt += strlen(s);
+					break;
+
+				default:
+					break;
+			}
+
+		}
+
+		else {
+			*out = *fmt;
+			out ++;
+			cnt ++;
+		}
+
+		fmt ++;
+	}
+	
+	va_end(ap);
+	*out = '\0';
+
+	return cnt;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
