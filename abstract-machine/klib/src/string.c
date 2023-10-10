8bb44eb1e67e17da2c19c4ab73c5b845be6d7ea7
tracer-ics2023 <tracer@njuics.org>
2023-10-10 08:40:46 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:40:46 up 18 min,  4 users,  load average: 0.27, 0.26, 0.20

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -24,32 +24,50 @@ char *strcpy(char *dst, const char *src) {
 char *strncpy(char *dst, const char *src, size_t n) {
 	size_t i=0;
 	
-	for (i=0; i<n & src[i]!='\0', i++) {
-		dest[i] = src[i];
+	for (i=0; i<n && src[i]!='\0'; i++) {
+		dst[i] = src[i];
 	}
-	for (; i<n; i++a) {
-		dest[i] = '\0';
+	for (; i<n; i++) {
+		dst[i] = '\0';
 	}
-	return dest;
+	return dst;
 }
 
 char *strcat(char *dst, const char *src) {
 	size_t i=0;
 	size_t len = strlen(dst);
 
-	for (j=0; src[j]!='\0'; j++) {
-		dst[len+j] = src[j];
+	for (i=0; src[i]!='\0'; i++) {
+		dst[len+i] = src[i];
 	}
-	dst[len+j] = '\0';
+	dst[len+i] = '\0';
 	return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+	size_t i=0;
+	for (i=0; s1[i]!='\0' && s2[i]!='\0'; i++) {
+		if (s1[i] != s2[i]) {
+			return s1[i]-s2[i];
+		}
+	}
+	if (s1[i] != '\0') { return s1[i]; }
+	else if (s2[i] != '\0') {	return -s2[i];	}
+	else {	return 0;	}
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+	size_t i=0;
+	for (i=0; i<n && s1[i]!='\0' && s2[i]!='\0'; i++) {
+		if (s1[i] != s2[i]) {
+			return s1[i]-s2[i];
+		}
+	}
+	if (i<n) {
+		if (s1[i] != '\0') { return s1[i];	}
+		else if (s2[i] != '\0') { return -s2[i];	}
+	}
+	return 0;
 }
 
 void *memset(void *s, int c, size_t n) {
