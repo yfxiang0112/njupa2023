01a78377f3798758627d28fa770cfbfcffdd66b7
tracer-ics2023 <tracer@njuics.org>
2023-10-11 11:23:18 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:23:18 up  1:33,  5 users,  load average: 0.12, 0.06, 0.04

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -33,9 +33,11 @@ static void welcome() {
         "If it is not necessary, you can disable it in menuconfig"));
   Log("Build time: %s, %s", __TIME__, __DATE__);
 
+#ifdef CONFIG_PRLOGO
 	printf(ANSI_FMT(" ===", ANSI_FG_RED)ANSI_FMT("===", ANSI_FG_YELLOW)ANSI_FMT("===", ANSI_FG_GREEN)ANSI_FMT("===", ANSI_FG_BLUE)ANSI_FMT("===", ANSI_FG_CYAN)ANSI_FMT("===\n", ANSI_FG_MAGENTA));
 	printf(" coding with "ANSI_FMT("P", ANSI_FG_RED)ANSI_FMT("R", ANSI_FG_YELLOW)ANSI_FMT("I", ANSI_FG_GREEN)ANSI_FMT("D", ANSI_FG_BLUE)ANSI_FMT("E", ANSI_FG_CYAN)ANSI_FMT("!\n", ANSI_FG_MAGENTA));
 	printf(ANSI_FMT(" ===", ANSI_FG_RED)ANSI_FMT("===", ANSI_FG_YELLOW)ANSI_FMT("===", ANSI_FG_GREEN)ANSI_FMT("===", ANSI_FG_BLUE)ANSI_FMT("===", ANSI_FG_CYAN)ANSI_FMT("===\n", ANSI_FG_MAGENTA));
+#endif
 	
   printf("Welcome to %s-NEMU!\n", ANSI_FMT(str(__GUEST_ISA__), ANSI_FG_YELLOW ANSI_BG_RED));
   printf("For help, type \"help\"\n");
