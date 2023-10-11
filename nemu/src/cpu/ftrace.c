a8d8c62b5b46efb4014ce027da8f361eb69608c6
tracer-ics2023 <tracer@njuics.org>
2023-10-11 18:39:40 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:39:40 up  8:50,  6 users,  load average: 0.08, 0.39, 0.60

--- nemu/src/cpu/ftrace.c
+++ nemu/src/cpu/ftrace.c
@@ -87,12 +87,12 @@ void rec_ftrace(vaddr_t addr, vaddr_t pc, uint32_t inst_val) {
 
 	for (int i=0; i<func_num; i++) {
 		if (addr == funct_tab[i].addr) {
-			for (int j=0; j<call_cnt; j++) { printf(" "); }
-			printf("call [%s @0x%d]\n", funct_tab[i].name, addr);
+//			for (int j=0; j<call_cnt; j++) { printf(" "); }
+			printf("call [%s %*s @0x%d]\n", funct_tab[i].name, call_cnt, " ", addr);
 			call_cnt ++;
 			return;
 		}
 	}
-}
 #endif
+}
 
