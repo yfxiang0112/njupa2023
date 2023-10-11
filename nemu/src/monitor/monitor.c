3a83e5e3a3b110edc7b3d172eb5b13de6a5d0fd1
tracer-ics2023 <tracer@njuics.org>
2023-10-11 11:17:19 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:17:19 up  1:27,  4 users,  load average: 0.13, 0.09, 0.06

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -183,17 +183,17 @@ void init_ftrace(const char* elf_file) {
 	if (fp == NULL) { panic("elf file not found"); }
 
 	Elf32_Ehdr header;
-	Elf32_Shdr *sections = NULL;
-	Elf32_Sym *symtab = NULL;
 
 	succ = fread(&header, sizeof(Elf32_Ehdr), 1, fp);
 	if (!succ) { panic("fail to read head"); }
 	if (header.e_ident[0]!=0x7f||header.e_ident[1]!='E'||header.e_ident[2]!='L'||header.e_ident[3]!='F') { panic("not an elf file. "); }
 
+	Elf32_Shdr *sections = (Elf32_Shdr*)malloc(sizeof(Elf32_Shdr)*header.e_shnum);
 	succ = fread(sections, sizeof(Elf32_Shdr)*header.e_shnum, 1, fp);
 	if (!succ){ panic("fail to read sections"); }
 	//sections = (Elf32_Shdr *)((char*)&header + header.e_shoff);
 
+	Elf32_Sym *symtab = NULL;
 	for (int i=0; i<header.e_shnum; i++) {
 		printf("sh type %d = %d\n", i, sections[i].sh_type);
 		if (sections[i].sh_type == SHT_SYMTAB) {
