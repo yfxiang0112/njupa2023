db49e3724ac4013947cc3d4abda827b1bb468570
tracer-ics2023 <tracer@njuics.org>
2023-10-12 09:24:21 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:24:21 up 20 min,  5 users,  load average: 0.16, 0.13, 0.13

--- nemu/src/isa/loongarch32r/difftest/dut.c
+++ nemu/src/isa/loongarch32r/difftest/dut.c
@@ -18,7 +18,15 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+	if (pc != cpu.pc) {
+		return false;
+	}
+	for (int i=0; i<32; i++) {
+		if ref_r[i] != cpu.gpr[i] {
+			return false;
+		}
+	}
+  return true;
 }
 
 void isa_difftest_attach() {
