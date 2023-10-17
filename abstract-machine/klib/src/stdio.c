2c1434ec895587cd9fd05d78a09f14a29948b9d3
221300012-Xiang Yuanfang <yf.xiang@smail.nju.edu.cn>
2023-10-17 22:58:05 +0800

    printf temp ver

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -43,18 +43,70 @@ char* itoa(int num, char* buf, size_t base) {
 	return buf;
 }
 
-char* parse_fmt(const char* fmt, va_list ap, int *cnt) {
+char* ftoa(double num, char* buf) {
+	size_t arr[64] = {0};
+	size_t i = 0, len = 0;
+
+	if (num==0) {
+		*buf = 0;
+		*(buf+1) = '\0';
+		return buf;
+	}
+
+	if (num<0) {
+		*buf = '-';
+		buf ++;
+		num = -num;
+	}
+
+  int dec = (int) num;
+  num = num - (double) dec;
+
+  itoa(dec, buf, 10);
+
+  buf += strlen(buf);
+  *buf = '.';
+  buf ++;
+
+	while (num>0 && i<64) {
+    num *= 10;
+		arr[i] = (int)num;
+		num -= arr[i];
+		i++;
+	}
+  len = i;
+
+	for (i=0; i<len; i++) {
+		*buf = arr[i] + '0';
+		buf ++;
+	}
+	*buf = '\0';
+
+	return buf;
+}
+
+char* parse_fmt(const char** fmt, va_list ap, int *cnt) {
 
   int d;
+  double f;
 
-	if (*fmt == '%') {
-		fmt++;
+	if (**fmt == '%') {
+		*fmt = *fmt+1;
 
-		switch (*fmt) {
+		switch (**fmt) {
+      case '%':
+        break;
+
 			case 'd':
 				d = va_arg(ap, int);
         *cnt = *cnt+1;
 				itoa(d, fmt_buf, 10);
+        return fmt_buf;
+
+			case 'f':
+				f = va_arg(ap, double);
+        *cnt = *cnt+1;
+				ftoa(f, fmt_buf);
         return fmt_buf;
 
 			case 's':
@@ -67,7 +119,7 @@ char* parse_fmt(const char* fmt, va_list ap, int *cnt) {
 
 	}
 
-  singlech[0] = *fmt;
+  singlech[0] = **fmt;
   singlech[1] = '\0';
   return singlech;
 
@@ -76,7 +128,23 @@ char* parse_fmt(const char* fmt, va_list ap, int *cnt) {
 /////////////////////////////////////////
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+	va_list ap;
+	char *s;
+
+	int cnt = 0;
+
+	va_start(ap, fmt);
+	while (*fmt != '\0') {
+
+    s = parse_fmt(&fmt, ap, &cnt);
+    putstr(s);
+
+		fmt ++;
+	}
+	
+	va_end(ap);
+
+	return cnt;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
@@ -92,8 +160,7 @@ int sprintf(char *out, const char *fmt, ...) {
 	va_start(ap, fmt);
 	while (*fmt != '\0') {
 
-
-    s = parse_fmt(fmt, ap, &cnt );
+    s = parse_fmt(&fmt, ap, &cnt);
     memcpy(out, s, strlen(s));
 
     out += strlen(s);
