036ad3669f9228cdc7528af1b4d117dd4810772a
tracer-ics2023 <tracer@njuics.org>
2023-10-18 13:36:40 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:36:40 up  4:06,  5 users,  load average: 0.40, 0.29, 0.50

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,11 +1,15 @@
 #include <am.h>
 #include <nemu.h>
 
+#include <stdio.h>
+
 void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = ((uint64_t)inl(RTC_ADDR)<<32) | (inl(RTC_ADDR-32));
+  printf("%d\n", inl(RTC_ADDR));
+  //uptime->us = ((uint64_t)inl(RTC_ADDR)<<32) | (inl(RTC_ADDR-32));
+  uptime->us = 0;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
