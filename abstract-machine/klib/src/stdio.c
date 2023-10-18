42e6f763e888b83d7dbeb43ad8188b2d16cf6c93
tracer-ics2023 <tracer@njuics.org>
2023-10-18 18:06:47 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:06:47 up  8:36,  6 users,  load average: 0.62, 0.70, 0.75

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -112,7 +112,7 @@ char* parse_fmt(const char** fmt, va_list *ap, int *cnt) {
     if (wid==0) {putch('a');}
     if (wid > strlen(arg_buf)) { wid -= strlen(arg_buf); }
     else { wid = 0; }
-    strcpy(fmt_buf, arg_buf);
+    strcpy(fmt_buf+wid, arg_buf);
     return fmt_buf;
 
 	}
