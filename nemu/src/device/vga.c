b87fcc969302e7921ac83ec98484169c60d008fd
tracer-ics2023 <tracer@njuics.org>
2023-10-19 14:00:11 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:00:11 up  5:10,  5 users,  load average: 0.42, 0.26, 0.29

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -75,7 +75,11 @@ void vga_update_screen() {
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
   
-  printf("%x\n", vgactl_port_base[1]);
+  uint32_t sync = vgactl_port_base[1];
+  if (sync) {
+    update_screen(); 
+    vgactl_port_base[1] = 0;
+  }
   /*
   for (uint32_t x=0; x<screen_width(); x++) {
     for (uint32_t y=0; y<screen_height(); y++) {
