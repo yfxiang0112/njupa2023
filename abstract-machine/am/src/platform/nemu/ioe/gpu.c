6e4759044406719f40147a4258b1c00b18370eab
tracer-ics2023 <tracer@njuics.org>
2023-10-19 13:11:35 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:11:35 up  4:21,  5 users,  load average: 0.24, 0.51, 0.43

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -8,7 +8,8 @@ void __am_gpu_init() {
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   uint32_t gpuconfig = inl(VGACTL_ADDR);
-  printf("%d\n", gpuconfig);
+  printf("0x%x\n", gpuconfig);
+  printf("%d, %d\n", gpuconfig>>16, gpuconfig&0x00ff);
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
     .width = 0, .height = 0,
