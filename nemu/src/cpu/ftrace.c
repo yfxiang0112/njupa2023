430b62997a085ebeb91bced2a54e09adc971839b
tracer-ics2023 <tracer@njuics.org>
2023-10-19 09:16:11 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:16:11 up 26 min,  4 users,  load average: 0.40, 0.41, 0.41

--- nemu/src/cpu/ftrace.c
+++ nemu/src/cpu/ftrace.c
@@ -91,13 +91,13 @@ void rec_ftrace(vaddr_t addr, vaddr_t pc, uint32_t inst_val) {
 
 				/* using stack, pop function returns that are not recorded */
 				call_node *st_top_call = call_stack;
-        call_node *temp;
+        //call_node *temp;
 				while (st_top_call->func_ind != i) {
 					call_cnt--;
 					printf("0x%x%*s%s [%s]\n", pc, call_cnt, " ", ANSI_FMT("ret ", ANSI_FG_YELLOW), funct_tab[st_top_call->func_ind].name);
-          temp = st_top_call;
+          //temp = st_top_call;
 					st_top_call = st_top_call->next;
-          free(temp);
+          //free(temp);
 				}
 				call_stack = st_top_call -> next;
         //free(st_top_call);
