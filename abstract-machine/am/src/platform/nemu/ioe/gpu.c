9e7eb59c5a56d4b6002978ef6e7504aef7e73624
tracer-ics2023 <tracer@njuics.org>
2023-10-19 15:21:04 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:21:04 up  6:31,  5 users,  load average: 0.39, 0.53, 0.58

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,14 +4,6 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-  /*
-  int i;
-  int w = io_read(AM_GPU_CONFIG).width;
-  int h = io_read(AM_GPU_CONFIG).height;
-  uint32_t *fb = (uint32_t*)(uintptr_t)FB_ADDR;
-  for (i=0; i<w*h; i++) fb[i]=i;
-  outl(SYNC_ADDR, 1);
-  */
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -26,13 +18,11 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   uint32_t screen_w = inl(VGACTL_ADDR) >> 16; 
   
-//  uint32_t *fb = (uint32_t*)(uintptr_t)FB_ADDR;
   for (int y=0; y<ctl->h; y++) {
     for (int x=0; x<ctl->w; x++) {
-      uint32_t off_p = ((y * ctl->w) + x);// * sizeof(uint32_t);
+      uint32_t off_p = ((y * ctl->w) + x);
       uint32_t off_s = (((y+ctl->y) * screen_w) + (x+ctl->x)) * sizeof(uint32_t);
       outl(FB_ADDR+off_s, ((uint32_t*)(ctl->pixels))[off_p]);
-      //fb[off_s] = ((uint32_t*)(ctl->pixels))[off_p];
 
     }
   }
