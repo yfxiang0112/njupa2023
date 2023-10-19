d5d95f65205edaba37e4d6d09a4eb5bba69df86d
tracer-ics2023 <tracer@njuics.org>
2023-10-19 09:55:19 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:55:19 up  1:05,  5 users,  load average: 1.12, 0.64, 0.52

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,6 +5,6 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t curr_key = inw(KBD_ADDR);
-  kbd->keycode = curr_key <<8 >>8;
+  kbd->keycode = (curr_key <<8) >>8;
   kbd->keydown = curr_key>>15;
 }
