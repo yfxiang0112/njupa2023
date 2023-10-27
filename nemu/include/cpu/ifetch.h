44330a4706c6646b5418b93c8dc95b738a754114
tracer-ics2023 <tracer@njuics.org>
2023-10-27 16:27:21 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:27:21 up  3:03,  6 users,  load average: 0.35, 0.29, 0.18

--- nemu/include/cpu/ifetch.h
+++ nemu/include/cpu/ifetch.h
@@ -17,7 +17,7 @@
 
 #include <memory/vaddr.h>
 
-static uint32_t inst_fetch(vaddr_t *pc, int len) {
+inline uint32_t inst_fetch(vaddr_t *pc, int len) {
   uint32_t inst = vaddr_ifetch(*pc, len);
   (*pc) += len;
   return inst;
