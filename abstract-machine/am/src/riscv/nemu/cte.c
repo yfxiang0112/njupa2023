58225b4290533c25721866d7298f9ff4f4c67aeb
tracer-ics2023 <tracer@njuics.org>
2023-11-05 19:02:01 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:02:01 up  9:58,  6 users,  load average: 0.51, 0.37, 0.29

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -5,6 +5,14 @@
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
+  for (int i=0; i<32; i++) {
+    printf("0x%x", c->gpr[i]);
+    if (i%4==3) {printf("\n");}
+  }
+  printf("mcause= 0x%x\n", c->mcause);
+  printf("mstatus= 0x%x\n", c->mstatus);
+  printf("mepc= 0x%x\n", c->mepc);
+
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
