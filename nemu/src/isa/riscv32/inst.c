eaaf833ccbfdfbe77933cec7d140e4bb34f37ba1
tracer-ics2023 <tracer@njuics.org>
2023-11-05 19:41:29 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:41:29 up 10:38,  6 users,  load average: 0.24, 0.23, 0.26

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -120,8 +120,10 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? 010 ????? 11100 11", csrrs  , I, 
           uint32_t temp=src1; if(rd!=0){R(rd)=CSR(imm);} CSR(imm)= CSR(imm) | temp);
 
-  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, s->dnpc = isa_raise_intr(R(MUXDEF(__riscv_e,15,17)), s->snpc)); 
+  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, 
+          s->dnpc = isa_raise_intr(R(MUXDEF(__riscv_e,15,17)), s->snpc)); 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
+  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , N, s->dnpc = CSR(SR_MEPC)); 
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
   INSTPAT_END();
 
