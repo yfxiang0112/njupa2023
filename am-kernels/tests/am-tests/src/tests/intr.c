ebc34d2596c39bbcb08168aee0660fcf8fccda77
tracer-ics2023 <tracer@njuics.org>
2023-11-05 14:49:59 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:49:59 up  5:46,  6 users,  load average: 0.63, 0.55, 0.44

--- am-kernels/tests/am-tests/src/tests/intr.c
+++ am-kernels/tests/am-tests/src/tests/intr.c
@@ -20,8 +20,8 @@ void hello_intr() {
   io_read(AM_INPUT_CONFIG);
   iset(1);
   while (1) {
-    printf("loop\n");
-    for (volatile int i = 0; i < 10000000; i++) ;
+    //printf("loop\n");
+    //for (volatile int i = 0; i < 10000000; i++) ;
     yield();
   }
 }
