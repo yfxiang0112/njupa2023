8e66c41fc71eb9a09f1857bf3f50101f835bfcec
tracer-ics2023 <tracer@njuics.org>
2023-11-06 21:24:03 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:24:03 up  3:51,  8 users,  load average: 0.41, 0.29, 0.24

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -12,26 +12,19 @@
 static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr elf;
   Elf_Phdr ph;
-  char *load_ptr;//, *ret_ptr=NULL;
+  char *load_ptr;
 
   ramdisk_read(&elf, 0, sizeof(Elf_Ehdr));
-  printf("0x%x, off=0x%x, num=0x%x\n", elf, elf.e_phoff, elf.e_phnum);
-  printf("0x%x\n", elf.e_ident[0]);
 
   for (int i=0; i<elf.e_phnum; i++) {
     ramdisk_read(&ph, elf.e_phoff + i*elf.e_phentsize, elf.e_phentsize);
-    printf("%d, type=0x%x, addr=0x%x, fsz=0x%x, msz=0x%x\n",
-           i, ph.p_type, ph.p_vaddr, ph.p_filesz, ph.p_memsz);
     if (ph.p_type == 1) {
       load_ptr = (char*) ph.p_vaddr;
-      //if (!ret_ptr) { ret_ptr = load_ptr; }
-      printf("ptr = 0x%x\n", load_ptr);
       ramdisk_read(load_ptr, ph.p_offset, ph.p_filesz);
       memset(load_ptr+ph.p_filesz, ph.p_memsz-ph.p_filesz, 0);
     }
   }
   
-  //TODO();
   return elf.e_entry;
 }
 
