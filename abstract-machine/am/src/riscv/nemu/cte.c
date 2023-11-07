56a553a4718f47e8df9ecdd70131d10e0b640251
tracer-ics2023 <tracer@njuics.org>
2023-11-07 09:19:35 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:19:35 up 29 min,  3 users,  load average: 0.12, 0.12, 0.12

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -8,7 +8,8 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case -1: ev.event = EVENT_YIELD; break;
+      case -1: ev.event = EVENT_YIELD;   break;
+      case  1: ev.event = EVENT_SYSCALL; break;
       default: 
         printf("Unkown mcause code: %d\n", c->mcause);
         ev.event = EVENT_ERROR; 
--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,6 +3,7 @@
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD: printf("nanos-lite met yield event.\n"); break;
+    case EVENT_SYSCALL: printf("SYSCALL\n"); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
