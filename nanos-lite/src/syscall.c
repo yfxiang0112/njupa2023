87776c70d15e8c222255cbf0160ab3a7ca73b3e5
tracer-ics2023 <tracer@njuics.org>
2023-11-07 10:14:41 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:14:41 up  1:24,  3 users,  load average: 0.21, 0.22, 0.14

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,10 +1,13 @@
 #include <common.h>
 #include "syscall.h"
+#define SYS_yield 1
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
   switch (a[0]) {
+    case SYS_yield: yield(); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
