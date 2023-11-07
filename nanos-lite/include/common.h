8a0be0130279be04931e30b6c84828c5d670ed28
tracer-ics2023 <tracer@njuics.org>
2023-11-07 10:10:01 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:10:01 up  1:19,  3 users,  load average: 0.12, 0.11, 0.09

--- nanos-lite/include/common.h
+++ nanos-lite/include/common.h
@@ -17,4 +17,6 @@ size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 size_t get_ramdisk_size();
 
+void do_syscall(Context *c);
+
 #endif
--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,8 +2,15 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case EVENT_YIELD: printf("nanos-lite met yield event.\n"); break;
-    case EVENT_SYSCALL: printf("SYSCALL\n"); break;
+    case EVENT_YIELD: 
+      printf("nanos-lite met yield event.\n");
+      break;
+
+    case EVENT_SYSCALL:
+      printf("SYSCALL\n");
+      do_syscall(c);
+      break;
+
     default: panic("Unhandled event ID = %d", e.event);
   }
 
