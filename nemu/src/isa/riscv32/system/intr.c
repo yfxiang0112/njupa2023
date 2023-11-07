54a0b72ccbae42e4d154fc8f5d11e4aeccf431cb
tracer-ics2023 <tracer@njuics.org>
2023-11-07 14:13:13 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:13:13 up  5:22,  4 users,  load average: 0.27, 0.28, 0.32

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -18,7 +18,8 @@
 
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
 #ifdef CONFIG_ETRACE
-  printf("[ETRACE]: type %2d, at addr 0x%08x, jump to 0x%08x\n", NO, epc, csr(SR_MTVEC));
+  printf("%s type %2d, at addr 0x%08x, jump to 0x%08x\n",
+         ANSI_FMT("[ETRACE]:", ANSI_FG_YELLOW), NO, epc, csr(SR_MTVEC));
 #endif
 
   csr(SR_MEPC) = epc;
