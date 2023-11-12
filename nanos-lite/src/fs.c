1abaca6f7897b4b1c66c2a1441e15aec3c5329e1
tracer-ics2023 <tracer@njuics.org>
2023-11-12 20:41:02 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:41:02 up  8:53,  6 users,  load average: 0.09, 0.12, 0.16

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -7,6 +7,7 @@ typedef struct {
   char *name;
   size_t size;
   size_t disk_offset;
+  size_t open_offset;
   ReadFn read;
   WriteFn write;
 } Finfo;
@@ -25,9 +26,9 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+  [FD_STDIN]  = {"stdin", 0, 0, 0, invalid_read, invalid_write},
+  [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, invalid_write},
+  [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, invalid_write},
 #include "files.h"
 };
 
@@ -41,26 +42,50 @@ void init_fs() {
 int fs_open(const char *pathname, int flags, int mode) {
   for (int idx=0; idx<NR_FILES; idx++) {
     if (strcmp(file_table[idx].name , pathname) == 0) {
+      file_table[idx].open_offset = 0;
+      file_table[idx].read = ramdisk_read;
+      file_table[idx].write = ramdisk_write;
+
       return idx;
     }
   }
-    assert(0);
+
+  printf("cannot find requested file\n");
+  assert(0);
   return 2;
 }
 
 int fs_read(int fd, void* buf, size_t len) {
+  size_t off; 
+  assert(fd>2 && fd<NR_FILES);
+  if (file_table[fd].open_offset + len > file_table[fd].size) { panic("file operation exceed max size"); }
 
-  return 0;
+  off = file_table[fd].disk_offset + file_table[fd].open_offset;
+  file_table[fd].read(buf, off, len);
+  return len;
 }
 
 int fs_write(int fd, const void* buf, size_t len) {
+  assert(fd>2 && fd<NR_FILES);
+  if (file_table[fd].open_offset + len > file_table[fd].size) { panic("file operation exceed max size"); }
 
   return 0;
 }
 
 int fs_lseek(int fd, size_t offset, int whence) {
+  assert(fd>2 && fd<NR_FILES);
+
+  if (whence == SEEK_SET) {
+    if (offset > file_table[fd].size || offset < 0) { panic("file operation exceed max size"); }
+    file_table[fd].open_offset = offset;
+  }
+  else if (whence == SEEK_CUR) {
+    size_t cur = file_table[fd].open_offset;
+    if (offset+cur > file_table[fd].size || offset+cur < 0) { panic("file operation exceed max size"); }
+    file_table[fd].open_offset += offset;
+  }
     
-  return 0;
+  return offset;
 }
 
 int fs_close(int fd) {
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -12,9 +12,12 @@
 static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr elf;
   Elf_Phdr ph;
+  size_t fd;
   char *load_ptr;
 
-  ramdisk_read(&elf, 0, sizeof(Elf_Ehdr));
+  fd = fs_open(filename, 0, 0);
+
+  fs_read(fd, &elf, sizeof(Elf_Ehdr));
 
   assert(elf.e_ident[0] == 0x7f);
   assert(elf.e_ident[1] == 0x45);
@@ -22,12 +25,13 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   assert(elf.e_ident[3] == 0x46);
 
   for (int i=0; i<elf.e_phnum; i++) {
-    printf("fd=%d\n",fs_open(filename, 0, 0));
-    ramdisk_read(&ph, elf.e_phoff + i*elf.e_phentsize, elf.e_phentsize);
+    fs_lseek(fd, elf.e_phoff + i*elf.e_phentsize, 0);
+    fs_read(fd, &ph, elf.e_phentsize);
 
     if (ph.p_type == 1) {
       load_ptr = (char*) ph.p_vaddr;
-      ramdisk_read(load_ptr, ph.p_offset, ph.p_filesz);
+      fs_lseek(fd, ph.p_offset, 0);
+      fs_read(fd, load_ptr, ph.p_filesz);
       memset(load_ptr+ph.p_filesz, ph.p_memsz-ph.p_filesz, 0);
     }
   }
