72c4253eef44ac686de0fdd15e5a4e537f398477
tracer-ics2023 <tracer@njuics.org>
2023-11-12 10:40:37 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:40:37 up  1:42,  7 users,  load average: 1.17, 0.79, 0.60

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -71,9 +71,9 @@ int _write(int fd, void *buf, size_t count) {
 }
 
 void *_sbrk(intptr_t increment) {
-  void *ret;
+  uintptr_t ret;
   _syscall_(SYS_brk, increment, &ret, 0);
-  return ret;
+  return (void *)ret;
   //return (void *)-1;
 }
 
