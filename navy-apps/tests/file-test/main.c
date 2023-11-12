0fd66710ee3f8d245e5b5a857b24567dbe5df76c
tracer-ics2023 <tracer@njuics.org>
2023-11-12 21:55:30 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:55:30 up 10:07,  7 users,  load average: 1.21, 0.94, 0.66

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -7,7 +7,7 @@ int main() {
 
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
-  printf("size=%d\n", size);
+  printf("size=%ld\n", size);
   assert(size == 5000);
 
   fseek(fp, 500 * 5, SEEK_SET);
