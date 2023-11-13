7f1aa2f77f50784e8eb0d0ff7a8fb9ab65674104
tracer-ics2023 <tracer@njuics.org>
2023-11-13 20:36:21 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:36:21 up 11:42,  6 users,  load average: 0.61, 0.41, 0.40

--- navy-apps/libs/libfixedptc/include/fixedptc.h
+++ navy-apps/libs/libfixedptc/include/fixedptc.h
@@ -143,7 +143,7 @@ static inline fixedpt fixedpt_mul(fixedpt A, fixedpt B) {
 
 /* Divides two fixedpt numbers, returns the result. */
 static inline fixedpt fixedpt_div(fixedpt A, fixedpt B) {
-	return (A << 8) / B + ( (A >>8 <<8) / B ) << 8;
+	return (A << 8) / B + ( (A >>24 <<24) / B ) << 8;
 }
 
 static inline fixedpt fixedpt_abs(fixedpt A) {
