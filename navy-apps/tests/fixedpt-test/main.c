3df9d152c0d9580fec58349104ac8bc7ef8e6a27
tracer-ics2023 <tracer@njuics.org>
2023-11-13 20:44:17 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:44:17 up 11:50,  6 users,  load average: 0.64, 0.31, 0.32

--- navy-apps/tests/fixedpt-test/main.c
+++ navy-apps/tests/fixedpt-test/main.c
@@ -7,7 +7,7 @@ int main() {
 
   assert(fixedpt_mul(fixedpt_rconst(1.1), fixedpt_rconst(1.1)) == fixedpt_rconst(1.21));
   assert(fixedpt_mul(fixedpt_rconst(-1.1), fixedpt_rconst(-1.1)) == fixedpt_rconst(1.21));
-  assert(fixedpt_mul(fixedpt_rconst(-1.1), fixedpt_rconst(1.1)) == fixedpt_rconst(-1.21));
+  assert(fixedpt_mul(fixedpt_rconst(-0.5), fixedpt_rconst(0.5)) == fixedpt_rconst(-0.25));
   assert(fixedpt_muli(fixedpt_rconst(1.21), 2) == fixedpt_rconst(2.42));
 
   assert(fixedpt_div(fixedpt_rconst(0.25), fixedpt_rconst(0.5)) == fixedpt_rconst(0.5));
