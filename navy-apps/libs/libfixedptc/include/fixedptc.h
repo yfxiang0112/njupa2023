1830498e5d9865040fad695260ef929c231f5b09
221300012-Xiang Yuanfang <yf.xiang@smail.nju.edu.cn>
2023-11-13 21:46:01 +0800

    nov.13 pa3.3 fixedptc-lib completed (to be tested)

--- navy-apps/libs/libfixedptc/include/fixedptc.h
+++ navy-apps/libs/libfixedptc/include/fixedptc.h
@@ -152,8 +152,6 @@ static inline fixedpt fixedpt_abs(fixedpt A) {
 }
 
 static inline fixedpt fixedpt_floor(fixedpt A) {
-  //if (A<0 && (fixedpt_fracpart(A) != 0)) {printf("check\n"); A = fixedpt_sub( A, FIXEDPT_ONE );}
-  //printf("%s\n", fixedpt_cstr(A, -1));
   return fixedpt_fromint(fixedpt_toint(A));
 }
 
