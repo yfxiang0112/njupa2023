120214e1e085e8db24ecd152eea3c986200bca45
tracer-ics2023 <tracer@njuics.org>
2023-11-13 11:16:42 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:16:42 up  2:23,  3 users,  load average: 0.68, 0.51, 0.36

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -6,14 +6,14 @@ extern int _gettimeofday(struct timeval *tv, struct timezone *tz);
 
 int main() {
 
-  struct timeval *tv = {0};
-  struct timezone *tz = {0};
+  struct timeval tv = {0};
+  struct timezone tz = {0};
   
   while (1) {
 
     //_syscall_(19, (uintptr_t)tv, (uintptr_t)tz, 0);
-    _gettimeofday(tv, tz);
-    printf("%ld\n", tv->tv_usec);
+    _gettimeofday(&tv, &tz);
+    printf("%ld\n", tv.tv_usec);
   }
 
   return 0;
