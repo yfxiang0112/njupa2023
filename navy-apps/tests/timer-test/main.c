ea890fbc4622d0a8cb94665ecbe3aa701c787ded
tracer-ics2023 <tracer@njuics.org>
2023-11-13 13:18:55 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:18:55 up  4:25,  4 users,  load average: 0.87, 0.70, 0.53

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -12,17 +12,23 @@ int main() {
   NDL_Init(0);
 
   printf("size=%d\n", sizeof(tv.tv_usec));
+
+  int sec = NDL_GetTicks() / 1000000;
   
   while (1) {
+
+    while(NDL_GetTicks() /100000 < sec);
 
     //_syscall_(19, (uintptr_t)tv, (uintptr_t)tz, 0);
     //_gettimeofday(&tv, &tz);
     //uint32_t usec = tv.tv_usec;
-    uint32_t usec = NDL_GetTicks();
-    if (usec % 1000 == 0) {
-      printf("%d\n", usec);
+    //uint32_t usec = NDL_GetTicks();
+    //if (usec % 1000000 == 0) {
+      printf("%d\n", sec);
       //printf("tick\n");
-    }
+    //}
+
+    sec ++;
   }
 
   return 0;
