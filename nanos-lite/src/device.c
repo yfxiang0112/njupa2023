89565f62634c72be8207548a675f3beb43ac8c12
tracer-ics2023 <tracer@njuics.org>
2023-11-13 14:57:37 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:57:37 up  6:04,  5 users,  load average: 1.31, 0.94, 1.14

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -20,7 +20,6 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-  printf("len=%d\n", len);
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
   if (ev.keycode == AM_KEY_NONE) return 0;
 
