3b0ba8c3be04aeb5b0ae3874c3e46aab495a8c20
tracer-ics2023 <tracer@njuics.org>
2023-11-13 14:32:21 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:32:21 up  5:38,  5 users,  load average: 1.79, 1.70, 1.39

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -28,6 +28,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   strcat(ev_str, keyname[ev.keycode]);
   memcpy(buf, ev_str, len);
 
+  printf("len=%d\n", len);
   printf("%s\n", (char*)buf +1);
   printf("%d\n", strlen(buf));
 
