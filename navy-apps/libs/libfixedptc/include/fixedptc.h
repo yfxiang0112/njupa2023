ecdd764df85d9d4d82816fd7e1c7c3a198289ee8
tracer-ics2023 <tracer@njuics.org>
2023-11-13 21:06:59 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:06:59 up 12:13,  6 users,  load average: 0.36, 0.52, 0.47

--- navy-apps/libs/libfixedptc/include/fixedptc.h
+++ navy-apps/libs/libfixedptc/include/fixedptc.h
@@ -152,7 +152,7 @@ static inline fixedpt fixedpt_abs(fixedpt A) {
 }
 
 static inline fixedpt fixedpt_floor(fixedpt A) {
-  if (A<0 && (fixedpt_fracpart(A) != 0)) {printf("check\n"); A = fixedpt_sub( A, FIXEDPT_ONE );}	
+  //if (A<0 && (fixedpt_fracpart(A) != 0)) {printf("check\n"); A = fixedpt_sub( A, FIXEDPT_ONE );}	
   return A>>8;
 }
 
