4c26ff88c91b927e47ef185825b95bfd052684ed
tracer-ics2023 <tracer@njuics.org>
2023-11-13 21:17:52 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:17:52 up 12:24,  6 users,  load average: 0.67, 0.51, 0.47

--- navy-apps/tests/fixedpt-test/main.c
+++ navy-apps/tests/fixedpt-test/main.c
@@ -23,7 +23,7 @@ int main() {
   assert(fixedpt_floor(fixedpt_rconst(-127.625)) == fixedpt_rconst(-128)); 
   assert(fixedpt_floor(fixedpt_rconst(0.5)) == fixedpt_rconst(0)); 
   assert(fixedpt_floor(fixedpt_rconst(128.625)) == fixedpt_rconst(128)); 
-  assert(fixedpt_floor(fixedpt_rconst(-128)) == fixedpt_rconst(128)); 
+  assert(fixedpt_floor(fixedpt_rconst(-128)) == fixedpt_rconst(-128)); 
   assert(fixedpt_floor(fixedpt_rconst(128)) == fixedpt_rconst(128)); 
   
   assert(fixedpt_ceil(fixedpt_rconst(0)) == fixedpt_rconst(0)); 
@@ -31,7 +31,7 @@ int main() {
   assert(fixedpt_floor(fixedpt_rconst(-128.625)) == fixedpt_rconst(-128)); 
   assert(fixedpt_floor(fixedpt_rconst(0.5)) == fixedpt_rconst(1)); 
   assert(fixedpt_floor(fixedpt_rconst(127.625)) == fixedpt_rconst(128)); 
-  assert(fixedpt_floor(fixedpt_rconst(-128)) == fixedpt_rconst(128)); 
+  assert(fixedpt_floor(fixedpt_rconst(-128)) == fixedpt_rconst(-128)); 
   assert(fixedpt_floor(fixedpt_rconst(128)) == fixedpt_rconst(128)); 
 
   return 0;
