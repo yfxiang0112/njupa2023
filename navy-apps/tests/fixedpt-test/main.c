e201da3b9c2bc0b3ee50db4f32869104b7fa8d33
tracer-ics2023 <tracer@njuics.org>
2023-11-13 20:18:19 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:18:19 up 11:24,  6 users,  load average: 0.43, 0.37, 0.36

--- navy-apps/tests/fixedpt-test/main.c
+++ navy-apps/tests/fixedpt-test/main.c
@@ -1,24 +1,11 @@
 #include <stdio.h>
 #include <assert.h>
 #include <sys/time.h>
-#include <NDL.h>
-
-extern int _gettimeofday(struct timeval *tv, struct timezone *tz);
+#include <fixedptc.h>
 
 int main() {
 
-  struct timeval tv = {0};
-  struct timezone tz = {0};
-  NDL_Init(0);
-
-  int sec = NDL_GetTicks() / 1000000;
-  
-  while (1) {
-    while(NDL_GetTicks() /1000000 < sec);
-
-    printf("%d\n", sec);
-    sec ++;
-  }
+  assert(fixedpt_mul(fixedpt_rconst(1.1), fixedpt_rconst(1.1)) == fixedpt_rconst(1.21));
 
   return 0;
 }
