b4dfbecc250089a27ad1dc83d08e31eb01f76ac2
tracer-ics2023 <tracer@njuics.org>
2023-11-13 09:51:26 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:51:26 up 57 min,  3 users,  load average: 0.48, 0.41, 0.40

--- nanos-lite/include/common.h
+++ nanos-lite/include/common.h
@@ -6,7 +6,7 @@
 //#define HAS_VME
 //#define MULTIPROGRAM
 //#define TIME_SHARING
-//#define ENABLE_STRACE
+#define ENABLE_STRACE
 //
 
 #include <am.h>
@@ -25,7 +25,6 @@ int fs_lseek(int fd, size_t offset, int whence);
 int fs_close(int fd);
 
 void do_syscall(Context *c);
-//
 // macro stringizing
 #define str_temp(x) #x
 #define str(x) str_temp(x)
