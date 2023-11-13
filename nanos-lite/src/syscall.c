bed0f0e4431bdd42dcbcf9a725d88030431bd787
tracer-ics2023 <tracer@njuics.org>
2023-11-13 11:48:33 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:48:33 up  2:55,  3 users,  load average: 0.30, 0.52, 0.42

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -51,8 +51,9 @@ void do_syscall(Context *c) {
       break;
 
     case SYS_gettimeofday:
-      ((struct timeval *)a[1])->tv_usec = io_read(AM_TIMER_UPTIME).us;
-      ((struct timeval *)a[1])->tv_sec = io_read(AM_TIMER_UPTIME).us / 1000;
+      uint32_t tick = io_read(AM_TIMER_UPTIME).us;
+      ((struct timeval *)a[1])->tv_usec = tick;
+      ((struct timeval *)a[1])->tv_sec = tick / 1000;
       c->GPRx = 0;
       break;
       
