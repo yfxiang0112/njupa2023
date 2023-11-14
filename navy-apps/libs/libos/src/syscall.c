5e273d315a5cd10e566eff2755d6a79c00022918
tracer-ics2023 <tracer@njuics.org>
2023-11-14 11:14:05 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:14:05 up  2:00,  5 users,  load average: 0.23, 0.22, 0.28

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -74,8 +74,11 @@ int _write(int fd, void *buf, size_t count) {
 }
 
 void *_sbrk(intptr_t increment) {
-  if(increment >= 0) memset((char*)(pb_addr), 0, increment);
-  else memset((char*)(pb_addr+increment), 0, -increment);
+  //if(increment >= 0) memset((char*)(pb_addr), 0, increment);
+  //else memset((char*)(pb_addr+increment), 0, -increment);
+  for (int i=0; i<increment; i++) {
+    printf("%x\n", *(uint32_t*)(pb_addr+i));
+  }
 
   uintptr_t pb_ret = pb_addr;
   pb_addr += (int32_t)increment;
