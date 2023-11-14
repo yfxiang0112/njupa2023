7105d3037a5a767bd1bd4506b5bcd6dfb149b4a4
tracer-ics2023 <tracer@njuics.org>
2023-11-14 09:38:59 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:38:59 up 25 min,  3 users,  load average: 0.78, 0.49, 0.29

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -13,8 +13,8 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
-  int off = s->w * y + x;
-  NDL_DrawRect((uint32_t*)(s->pixels + off), x, y, w, h);
+  //int off = s->w * y + x;
+  //NDL_DrawRect((uint32_t*)(s->pixels + off), x, y, w, h);
 }
 
 // APIs below are already implemented.
