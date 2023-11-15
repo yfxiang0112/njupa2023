e2cc6ad3f3ee781edcef9d5ccc69bdeaf03391f5
tracer-ics2023 <tracer@njuics.org>
2023-11-16 00:05:41 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:05:41 up  5:53,  5 users,  load average: 0.46, 0.34, 0.46

--- navy-apps/libs/libminiSDL/src/timer.c
+++ navy-apps/libs/libminiSDL/src/timer.c
@@ -4,10 +4,12 @@
 #include <assert.h>
 
 SDL_TimerID SDL_AddTimer(uint32_t interval, SDL_NewTimerCallback callback, void *param) {
+  assert(0);
   return NULL;
 }
 
 int SDL_RemoveTimer(SDL_TimerID id) {
+  assert(0);
   return 1;
 }
 
