f1c16365bf193604d22071b0817e582c92ee43f0
tracer-ics2023 <tracer@njuics.org>
2023-11-16 16:17:38 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:17:38 up 19 min,  3 users,  load average: 0.64, 0.91, 0.66

--- navy-apps/tests/array-test/main.c
+++ navy-apps/tests/array-test/main.c
@@ -5,9 +5,9 @@
 uint32_t foo() {
   uint32_t test[100000];
 
-  for (int i=0; i<85000; i++) {
-    test[i] = 0;
-    printf("0x%x , %d\n", i,test[i]);
+  for (int i=0; i<10000; i++) {
+    test[i] = 1;
+    //printf("0x%x , %d\n", i,test[i]);
   }
 
   assert(0);
