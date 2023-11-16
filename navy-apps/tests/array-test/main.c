3d4acd94df0b9dbb6b18fda1f52710e1a971b618
tracer-ics2023 <tracer@njuics.org>
2023-11-16 16:08:18 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:08:18 up 10 min,  3 users,  load average: 0.76, 0.67, 0.41

---
+++ navy-apps/tests/array-test/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <assert.h>
+#include <sys/time.h>
+
+uint32_t foo() {
+  uint32_t test[100000];
+
+  for (int i=0; i<100000; i++) {
+    test[i] = i;
+  }
+
+  for (int i=100000-1; i>=0; i--) {
+    printf("[%d]=%d\n", i, test[i]);
+    test[i] = 100000-i;
+  }
+
+  return test[5000];
+}
+
+uint32_t bar(int a) {
+  int b=1000;
+  uint32_t test[a*b];
+
+  for (int i=0; i<a*b; i++) {
+    printf("bar[%d] =%d\n", i, test[i]);
+    test[i] = 0xffffffff;
+  }
+
+  return a;
+}
+
+int main() {
+  
+  foo();
+
+  for (int i=0; i<10; i++) {
+    foo();
+    bar(i*1000);
+  }
+
+  bar(0xffff);
+
+  return 0;
+}
