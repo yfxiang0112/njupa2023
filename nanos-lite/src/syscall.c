9b3e86c38d36ac507e3ab806b74bd727a1151a9a
tracer-ics2023 <tracer@njuics.org>
2023-11-16 09:54:17 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:54:17 up  1:12,  4 users,  load average: 0.62, 0.56, 0.55

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -18,7 +18,7 @@ void do_syscall(Context *c) {
   switch (a[0]) {
     case SYS_exit:  
       //halt(a[1]);  
-      naive_uload(NULL, "/bin/menu");
+      naive_uload(NULL, "/bin/nterm");
       c->GPRx = 0;
       break;
 
