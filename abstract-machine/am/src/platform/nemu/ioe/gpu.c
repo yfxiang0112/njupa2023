022497dcd1e4af8619a95c3b3877e7f5691d1aa0
tracer-ics2023 <tracer@njuics.org>
2023-11-17 11:12:46 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:12:46 up  2:57,  5 users,  load average: 0.98, 0.60, 0.62

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -18,17 +18,15 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   uint32_t screen_w = inl(VGACTL_ADDR) >> 16; 
 
-  if (ctl->pixels == 0) {
-    printf("test\n");
-    return;
-  }
   
   for (int y=0; y<ctl->h; y++) {
     uint32_t row_off_p = y * ctl->w;
     uint32_t row_off_s = (y+ctl->y) * screen_w;
     for (int x=0; x<ctl->w; x++) {
-      printf("%x\n", (uintptr_t)ctl->pixels);
-      outl(FB_ADDR+(row_off_s+x+ctl->x)*sizeof(uint32_t), ((uint32_t*)(ctl->pixels))[row_off_p + x]);
+      if(ctl->pixels)
+        outl(FB_ADDR+(row_off_s+x+ctl->x)*sizeof(uint32_t), ((uint32_t*)(ctl->pixels))[row_off_p + x]);
+      else
+        outl(FB_ADDR+(row_off_s+x+ctl->x)*sizeof(uint32_t), 0);
     }
   }
   if (ctl->pixels && ctl->sync) {
