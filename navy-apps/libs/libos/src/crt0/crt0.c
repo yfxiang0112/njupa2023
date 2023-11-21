bf2008d890b0eb55a44e410daee24c7aa5e4683f
tracer-ics2023 <tracer@njuics.org>
2023-11-21 16:35:01 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:35:01 up  3:21,  3 users,  load average: 0.60, 0.48, 0.41

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -6,21 +6,16 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-  int argc = *args, n_env;
+
+  int argc = *args;
   char **argv = (char **)(args + 1);
   char **envp = (char **)(args + argc + 2);
-  for (int i=0; i<argc; i++) {
-    printf("%s %x\n", (char*)(argv[i]), (uintptr_t)(argv[i]));
-  }
-
-  for (int i=0; envp[i]!=NULL; i++) {
-    printf("%s %x\n", (char*)(envp[i]), (uintptr_t)(envp[i]));
-  }
-
-  //for (int i=0; i<)
 
   char *empty[] =  {NULL };
+
+  printf("%d", envp==empty);
+
   environ = empty;
-  exit(main(0, empty, empty));
+  exit(main(argc, argv, envp));
   assert(0);
 }
