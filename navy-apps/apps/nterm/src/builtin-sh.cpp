c2b7f9b9653b253500339eb06b26c172618559e7
tracer-ics2023 <tracer@njuics.org>
2023-11-21 21:04:14 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:04:14 up 4 min,  3 users,  load average: 1.34, 0.90, 0.40

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -62,8 +62,7 @@ static void sh_handle_cmd(const char *cmd) {
 void builtin_sh_run() {
   sh_banner();
   sh_prompt();
-  setenv("PATH", "/bin:$PATH", 0);
-  setenv("PATH", "/usr/bin:$PATH", 0);
+  setenv("PATH", "/usr/bin:/bin:$PATH", 0);
 
   while (1) {
     SDL_Event ev;
