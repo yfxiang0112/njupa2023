062ed800325e30f616ec80ad9ac7dbdccd9909b2
tracer-ics2023 <tracer@njuics.org>
2023-11-22 10:16:47 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:16:47 up 29 min,  3 users,  load average: 1.88, 1.67, 1.13

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -80,7 +80,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
       fb[off+i] = pixels[y*w+i];
     }
     //printf("x=%d, y=%d, w=%d, h=%d, cw=%d, ch=%d\n", x, y, w, h, canvas_w, canvas_h);
-    if (y == canvas_w-1) {
+    if (y == canvas_h-1) {
       printf("test\n");
       lseek(fbdev, 0, SEEK_SET);
       write(fbdev, fb, screen_w*screen_h);
