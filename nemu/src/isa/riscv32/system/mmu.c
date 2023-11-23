aa23a7e0c983718e9bc2e5f22987a3eae8bdaea5
tracer-ics2023 <tracer@njuics.org>
2023-11-23 13:33:35 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:33:35 up  4:54,  3 users,  load average: 0.19, 0.23, 0.34

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -19,9 +19,8 @@
 #include <memory/host.h>
 
 int isa_mmu_check(vaddr_t vaddr, int len, int type) {
-  //printf("satp=%x\n", cpu.csr[4]);
   if (cpu.csr[4]==0) { return MMU_DIRECT; }
-  if (cpu.csr[4] >> 31 == 1) { return MMU_TRANSLATE; }
+  if (cpu.csr[4] >> 31 == 1) { printf("satp=%x\n",cpu.csr[4]); return MMU_TRANSLATE; }
   return MMU_FAIL;
 }
 
