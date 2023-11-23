e3503bb43cf9fbfbe88435b509c0cb8567daee0b
tracer-ics2023 <tracer@njuics.org>
2023-11-23 16:43:34 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:43:33 up  8:04,  3 users,  load average: 0.45, 0.30, 0.17

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -88,13 +88,12 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
   // TODO: 2nd lvl pt?
   //
   pte = (ppn1>>2) | (ppn0>>2) | x | w | r | v;
-  if (pte==0x2840040f) printf("map: va=%x, pa=%x, pte=%x, ppn1=%x, ppn0=%x, ppn1>>2=%x\n", 
-                              va, pa, pte, ppn1, ppn0, ppn1>>2);
   // NOTE: for i=0 vaddr mapping
 
   //printf("pdir=%x, vpn0=%x, pte_addr=%x\n", pdir, vpn0, pdir * PGSIZE + vpn0 * PTESIZE );
   uintptr_t pte_addr = pdir * PGSIZE + vpn1 * PTESIZE;
   //assert(pte_addr <= (uintptr_t)(as->ptr) + PGSIZE); // NOTE: tmp test for equiv mapping
+  printf("%x\n", pte_addr);
   *(uintptr_t*)pte_addr = pte;
   //printf("test\n");
   
