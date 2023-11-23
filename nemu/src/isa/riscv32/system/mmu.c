8c4bb817c0455dbc29fda7b91ec2c71f7b918e41
tracer-ics2023 <tracer@njuics.org>
2023-11-23 13:17:27 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:17:27 up  4:38,  3 users,  load average: 0.22, 0.32, 0.29

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -33,8 +33,11 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   vpn0 = vaddr & 0x003ff000;
   voff = vaddr & 0x00000fff;
 
+
   vaddr_t satp_ppn = cpu.csr[4] & 0x003fffff;
   vaddr_t pte_addr = satp_ppn * 4096 + vpn0 * 4;
+  printf("vaddr=%x, satp=%x, pte_addr=%x\n", vaddr, cpu.csr[4], pte_addr);
+
   word_t pte = host_read(guest_to_host(pte_addr), 4);// TODO: mem read to get pte value
   ppn1 = pte   & 0xfff00000;
   ppn0 = pte   & 0x000ffc00;
