8327e339e270fb4926945077b63c28f33651fc88
tracer-ics2023 <tracer@njuics.org>
2023-11-24 20:08:20 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:08:20 up 11:32,  5 users,  load average: 0.36, 0.27, 0.25

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -62,7 +62,7 @@ uintptr_t loader(PCB *pcb, const char *filename) {
         }
         printf("pgend=%x, pastart=%x, fileend=%x, memend=%x\n", (uintptr_t)load_pg+PGSIZE, pa_start, pa_start+ph.p_filesz, pa_start+ph.p_memsz);
         printf("set_len=%x\n", ((uintptr_t)load_pg-pa_start-ph.p_filesz)%PGSIZE);
-        memset((char*)(pa_start+ph.p_filesz+1), 0, ph.p_memsz-ph.p_filesz);//(uintptr_t)load_pg+PGSIZE-pa_start-ph.p_filesz);
+        memset((char*)(pa_start+ph.p_filesz+1), 0, ((uintptr_t)load_pg-pa_start-ph.p_filesz)%PGSIZE);//(uintptr_t)load_pg+PGSIZE-pa_start-ph.p_filesz);
         pcb->max_brk = (uintptr_t)load_va;
         //pcb->max_brk = ph.p_vaddr + ph.p_memsz;
       } else {
