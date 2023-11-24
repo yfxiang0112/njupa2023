853ab9360d47ac24521ff7ed37ede677504faa96
tracer-ics2023 <tracer@njuics.org>
2023-11-24 18:30:36 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:30:36 up  9:55,  5 users,  load average: 0.63, 0.40, 0.27

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -76,7 +76,7 @@ int _write(int fd, void *buf, size_t count) {
 
 void *_sbrk(intptr_t increment) {
   //printf("sbrk assert\n");
-  printf("incr=%d\n", increment);
+  printf("%d\n", increment);
   assert(0);
   uintptr_t succ = _syscall_(SYS_brk, pb_addr, (intptr_t)increment, 0);
   
