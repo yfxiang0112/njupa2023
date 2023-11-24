5dd3d003f17bf36c54ed207f0008ee34f653ef12
tracer-ics2023 <tracer@njuics.org>
2023-11-24 09:45:17 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:45:17 up  1:09,  3 users,  load average: 0.77, 0.47, 0.32

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -85,6 +85,7 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
   pte1_addr = pdir * PGSIZE + (vpn1>>22) * PTESIZE;
 
   if ((uintptr_t)va < 0x80000000) printf("test:87\n");
+  printf("pte1_addr = %x\n", pte1_addr);
 
   if (*(uintptr_t*)pte1_addr == 0) {
     pte0_addr = (uintptr_t) pgalloc_usr(PGSIZE);
