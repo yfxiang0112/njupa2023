27dd87c6b85a95a97510c347087c7cdbb989cd61
tracer-ics2023 <tracer@njuics.org>
2023-11-24 09:48:13 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:48:13 up  1:12,  3 users,  load average: 0.29, 0.37, 0.31

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -42,6 +42,7 @@ void context_kload(PCB* n_pcb, void (*entry)(void *), void *arg) {
 
 size_t context_uload(PCB* n_pcb, const char* filename, char *const argv[], char *const envp[]) {
   //fb_write(NULL, 0, io_read(AM_GPU_CONFIG).width*io_read(AM_GPU_CONFIG).width);
+  protect(&(n_pcb->as));
   io_write(AM_GPU_FBDRAW, 0, 0, NULL, io_read(AM_GPU_CONFIG).width, io_read(AM_GPU_CONFIG).height, true);
   void *new_stack = new_page(8);
   uintptr_t usp = (uintptr_t)(new_stack + 8*PGSIZE - 1);
