8bd260478766d6a7ee6dee61aa7775907b7deb89
tracer-ics2023 <tracer@njuics.org>
2023-11-24 15:57:56 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:57:56 up  7:22,  4 users,  load average: 0.27, 0.13, 0.16

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -62,7 +62,7 @@ void do_syscall(Context *c) {
       //TODO: 
      
       printf("brk=%x, inc=%d, a3=%x\n", a[1], a[2], a[3]);
-      if (a[2] > 0x8000000) { c->GPRx=0; break; }
+      if (a[2] > 0x8000000) { c->GPRx=-1; break; }
       assert(0);
       for (uint32_t i=0; i<(int32_t)a[2]; i++) {
         *(uint32_t*)(a[1] + i) = 0;
