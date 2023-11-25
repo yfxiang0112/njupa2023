caf91b1911122142b30d90e43ef031a589c39115
tracer-ics2023 <tracer@njuics.org>
2023-11-25 20:32:10 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:32:10 up  5:11,  3 users,  load average: 0.48, 0.48, 0.43

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -20,8 +20,8 @@ struct BitmapHeader {
 } __attribute__((packed));
 
 void* BMP_Load(const char *filename, int *width, int *height) {
-  asm volatile("ebreak");
   FILE *fp = fopen(filename, "r");
+  asm volatile("ebreak");
   if (!fp) return NULL;
 
   struct BitmapHeader hdr;
