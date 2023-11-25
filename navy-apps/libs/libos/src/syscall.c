096ed2a83e519fc8b0225a51adddf5390c59beb6
tracer-ics2023 <tracer@njuics.org>
2023-11-25 20:10:16 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:10:16 up  4:49,  3 users,  load average: 0.31, 0.37, 0.35

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -52,6 +52,7 @@ extern char end;
 uintptr_t pb_addr = (uintptr_t)(&end);
 
 intptr_t _syscall_(intptr_t type, intptr_t a0, intptr_t a1, intptr_t a2) {
+  asm volatile ("ebreak");
   register intptr_t _gpr1 asm (GPR1) = type;
   register intptr_t _gpr2 asm (GPR2) = a0;
   register intptr_t _gpr3 asm (GPR3) = a1;
