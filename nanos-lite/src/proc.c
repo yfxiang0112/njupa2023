5ee5f59794e8d90188b5c862c8d2bb6378e3e161
tracer-ics2023 <tracer@njuics.org>
2023-11-25 21:46:49 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:46:49 up  6:26,  5 users,  load average: 0.33, 0.23, 0.26

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -107,7 +107,7 @@ size_t context_uload(PCB* n_pcb, const char* filename, char *const argv[], char 
 
   
   for (uintptr_t i=usp; i<(uintptr_t)(new_stack + 8*PGSIZE - 1); i++) {
-    printf("%02x %c   ", *((char*)i), *((char*)i));
+    printf("%x %02x %c\n", i, *((char*)i), *((char*)i));
   }
   return 0;
   
