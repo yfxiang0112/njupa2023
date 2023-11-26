1a65db4b96c2b143f43a23cd8048486dbdb0828e
tracer-ics2023 <tracer@njuics.org>
2023-11-26 11:46:57 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:46:57 up 56 min,  3 users,  load average: 0.94, 0.40, 0.24

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -28,7 +28,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   return csr(SR_MTVEC);
 }
 
-word_t isa_quljery_intr() {
+word_t isa_query_intr() {
   if (cpu.intr && (cpu.csr[2]&0b100)) {
     cpu.intr = false;
     return IRQ_TIMER;
