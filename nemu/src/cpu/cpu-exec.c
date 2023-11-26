8f7b934d922e603e6d811b7dc2be2ba37e7c061f
tracer-ics2023 <tracer@njuics.org>
2023-11-26 11:37:15 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:37:15 up 46 min,  3 users,  load average: 0.47, 0.30, 0.19

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -100,7 +100,9 @@ static void execute(uint64_t n) {
 
     word_t intr = isa_query_intr();
     if (intr != INTR_EMPTY) {
-      cpu.pc = isa_raise_intr(intr, cpu.pc);
+      vaddr_t entry = isa_raise_intr(intr, cpu.pc);
+      printf("event entry=%x\n", entry);
+      cpu.pc = entry;
     }
   }
 
