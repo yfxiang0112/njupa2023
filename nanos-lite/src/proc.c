c16b5780ab4e70c7677ba699f81b4f0b72288654
tracer-ics2023 <tracer@njuics.org>
2023-11-27 17:12:11 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:12:11 up  8:11,  6 users,  load average: 0.86, 0.56, 0.37

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -14,7 +14,7 @@ void switch_boot_pcb() {
 void hello_fun(void *arg) {
   int j = 1;
   while (1) {
-    Log("Hello World from Nanos-lite with arg '%p' for the %dth time!", (uintptr_t)arg, j);
+    //Log("Hello World from Nanos-lite with arg '%p' for the %dth time!", (uintptr_t)arg, j);
     j ++;
     yield();
   }
