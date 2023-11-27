abe705b940aa35d92e9e1e3dbb52c4269cd54e86
tracer-ics2023 <tracer@njuics.org>
2023-11-27 19:43:21 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:43:21 up 10:42,  6 users,  load average: 0.68, 0.46, 0.43

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -148,9 +148,10 @@ Context* schedule(Context *prev) {
   }
   */
   events_read(ev_buf, 0, 31);
+  printf("%s\n", ev_buf);
   if (strcmp(ev_buf, "kd F1") == 0) { fg_pcb = 1; }
-  if (strcmp(ev_buf, "kd F2") == 0) { fg_pcb = 1; }
-  if (strcmp(ev_buf, "kd F3") == 0) { fg_pcb = 1; }
+  if (strcmp(ev_buf, "kd F2") == 0) { fg_pcb = 2; }
+  if (strcmp(ev_buf, "kd F3") == 0) { fg_pcb = 3; }
   curr_idx = schedule_cnt==0 ? 0 : fg_pcb;
   schedule_cnt = (schedule_cnt + 1) % 2000;
 
