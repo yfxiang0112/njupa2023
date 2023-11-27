b2956d81f8442339174fc8f782f3b394aea22332
tracer-ics2023 <tracer@njuics.org>
2023-11-27 19:52:08 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:52:08 up 10:51,  6 users,  load average: 0.86, 0.66, 0.57

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -54,7 +54,8 @@ int main() {
 
   while (1) {
     SDL_Event e;
-    SDL_WaitEvent(&e);
+    //SDL_WaitEvent(&e);
+    SDL_PollEvent(&e);
 
     if (e.type == SDL_KEYDOWN) {
       switch(e.key.keysym.sym) {
@@ -68,6 +69,7 @@ int main() {
         case SDLK_7: rep = rep * 10 + 7; break;
         case SDLK_8: rep = rep * 10 + 8; break;
         case SDLK_9: rep = rep * 10 + 9; break;
+        case SDLK_Q: SDL_Quit();
         case SDLK_J:
         case SDLK_DOWN: next(rep); rep = 0; g = 0; break;
         case SDLK_K:
