4b6dfb42aff3422a1678cf2b0d5017537542fb6b
tracer-ics2023 <tracer@njuics.org>
2023-11-27 15:40:57 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:40:57 up  6:39,  3 users,  load average: 0.46, 0.29, 0.20

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,14 +15,14 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  //yield();
+  yield();
 
   for (int i=0; i<len; i++) { putch(*((char*)buf + i)); }
   return len;
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-  //yield();
+  yield();
 
   size_t ret;
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
@@ -40,7 +40,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 size_t fb_write(const void *buf, size_t offset, size_t len) {
   // TODO: Optimization need towards yield for each call
   
-  //yield();
+  yield();
 
   int w = io_read(AM_GPU_CONFIG).width;
   int h = io_read(AM_GPU_CONFIG).height;
