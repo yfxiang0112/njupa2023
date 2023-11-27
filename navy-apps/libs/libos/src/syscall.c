e8d51d27641301a3f73275d46f31e5ad963ebc90
tracer-ics2023 <tracer@njuics.org>
2023-11-27 19:24:30 +0800

    >  compile NEMU 221300012 项远方 Linux Lenovo-Yoga-3-11 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:24:30 up 10:23,  6 users,  load average: 0.38, 0.32, 0.40

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -123,6 +123,7 @@ int _fstat(int fd, struct stat *buf) {
 }
 
 int _stat(const char *fname, struct stat *buf) {
+  assert(0);
   return -1;
 }
 
@@ -137,34 +138,42 @@ pid_t _getpid() {
 }
 
 pid_t _fork() {
+  assert(0);
   return -1;
 }
 
 pid_t vfork() {
+  assert(0);
   return -1;
 }
 
 int _link(const char *d, const char *n) {
+  assert(0);
   return -1;
 }
 
 int _unlink(const char *n) {
+  assert(0);
   return -1;
 }
 
 pid_t _wait(int *status) {
+  assert(0);
   return -1;
 }
 
 clock_t _times(void *buf) {
+  assert(0);
   return 0;
 }
 
 int pipe(int pipefd[2]) {
+  assert(0);
   return -1;
 }
 
 int dup(int oldfd) {
+  assert(0);
   return -1;
 }
 
@@ -173,14 +182,17 @@ int dup2(int oldfd, int newfd) {
 }
 
 unsigned int sleep(unsigned int seconds) {
+  assert(0);
   return -1;
 }
 
 ssize_t readlink(const char *pathname, char *buf, size_t bufsiz) {
+  assert(0);
   return -1;
 }
 
 int symlink(const char *target, const char *linkpath) {
+  assert(0);
   return -1;
 }
 
